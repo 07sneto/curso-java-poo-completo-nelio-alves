a51cad56079ffe152743625b384d3bb7cd6a37a0
Severino Neto <severinoneto591@gmail.com>
2026-09-20 22:33:46 -0300

    Add files via upload

---
+++ exerciciosLogicaDeProg/src/estruturaRepetitivaFor/Exercicio01.java
@@ -0,0 +1,21 @@
+package estruturaRepetitivaFor;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int valor = scanner.nextInt();
+		
+		for (int i=1; i<=valor; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		
+		scanner.close();
+	}
+}
